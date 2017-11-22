4b11aa09a3f87b2c0de8251ad83cac53e031a727
safemp3link <31084941+ardiozan@users.noreply.github.com>
2017-11-22 11:55:34 +0700

    Update js-multi.js

--- js-multi.js
+++ js-multi.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="11/the-7-healthiest-foods-on-planet.html"}
 if(x=="2"){xxx="11/forex-tutorial-what-is-forex-trading.html"}
 if(x=="3"){xxx="11/what-is-health-insurance.html"}
-if(x=="4"){xxx="11/hard-to-believe-some-consumers-find.html"}a_to_ve[i].href="https://www.saraflink.tk/2017/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="11/hard-to-believe-some-consumers-find.html"}a_to_ve[i].href="http://www.saraflink.tk/2017/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
